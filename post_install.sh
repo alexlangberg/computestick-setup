1a227abbfde6403b80703b047e179edac90aeea1
Alex Langberg <al@ordrestyring.dk>
2015-10-09 10:01:05 +0200

    First commit.

---
+++ post_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
+
+# while installing:
+# disable update-notifier under "Startup Applications"
+# set autologin to 10 sec
+# set Grub timeout to 5 sec in two places
+# disable screen lock
+# disable screensaver
+
+apt-get remove libreoffice-* -y
+apt-get remove chromium-browser -y
+
+apt-get update
+apt-get -y upgrade
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+dpkg --install google-chrome-stable_current_amd64.deb
+apt-get install -f
+rm -f google-chrome-stable_current_amd64.deb
+
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer
+
+apt-get install -y openssh-server
+apt-get install -f
+apt-get autoremove -y
+
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+google-chrome --kiosk --disable-session-crashed-bubble --disable-infobars https://app.plecto.com/screen/de52ae924b7943dca7549e56ef980144
