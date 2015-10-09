ce89988ecf08a9ce23bd31669a23127eb0a16af2
Alex Langberg <al@ordrestyring.dk>
2015-10-09 15:03:51 +0200

    Disable translation.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-google-chrome --kiosk --disable-session-crashed-bubble --disable-infobars https://app.plecto.com/screen/de52ae924b7943dca7549e56ef980144
+google-chrome --kiosk --disable-session-crashed-bubble --disable-infobars --disable-translate https://app.plecto.com/screen/de52ae924b7943dca7549e56ef980144
