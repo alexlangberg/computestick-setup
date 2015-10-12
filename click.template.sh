f96823433adaef4a098d7aefd1d23ae1970c5786
Alex Langberg <al@ordrestyring.dk>
2015-10-12 11:05:24 +0200

    Move mouse away.

--- click.template.sh
+++ click.template.sh
@@ -1,4 +1,4 @@
 DISPLAY=:0
 
 # to click somewhere after 60 seconds
-# sleep 60 && xdotool mousemove 1920 975 && xdotool click 1
+# sleep 60 && xdotool mousemove 1920 975 && xdotool click 1 && xdotool mousemove 1920 1080
