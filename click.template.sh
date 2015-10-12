9185e097f51eadf3ff94b44e126f19cf5c2113e5
Alex Langberg <al@ordrestyring.dk>
2015-10-12 10:57:12 +0200

    Make click work.

--- click.template.sh
+++ click.template.sh
@@ -1,2 +1,4 @@
+DISPLAY=:0
+
 # to click somewhere after 60 seconds
 # sleep 60 && xdotool mousemove 1920 975 && xdotool click 1
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 source stick.sh
 source /home/ordrestyring/computestick-setup/stick.sh
 
-google-chrome --kiosk --disable-session-crashed-bubble --disable-infobars --disable-translate $STICK_URL
+google-chrome --kiosk --disable-session-crashed-bubble --disable-infobars --disable-translate $STICK_URL &
 
 source click.sh
 source /home/ordrestyring/computestick-setup/click.sh
