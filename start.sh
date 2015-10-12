adcf52018e136dc02ba54415695be9145cdae99f
Alex Langberg <al@ordrestyring.dk>
2015-10-12 13:12:51 +0200

    Change startup.

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ source /home/ordrestyring/computestick-setup/stick.sh
 
 export DISPLAY=:0
 
-google-chrome $STICK_CHROME_PARAMS --disable-session-crashed-bubble --disable-infobars --disable-translate $STICK_URL &
+google-chrome $STICK_CHROME_PARAMS --start-fullscreen --disable-session-crashed-bubble --disable-infobars --disable-translate $STICK_URL &
 
-sleep 60 && xdotool key F11 && sleep 3 && source /home/ordrestyring/computestick-setup/click.sh || source click.sh
+xdotool mousemove 1920 1080
+
+sleep 60 && source /home/ordrestyring/computestick-setup/click.sh && source click.sh
